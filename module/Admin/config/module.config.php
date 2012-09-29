1973624ec124db60cad3dc211b2979ce6460830d
Mizuki Yamanaka <charles.vineyard@gmail.com>
2012-09-30 01:29:58 +0900

    管理側のスケルトンを作成
    MLリスト画面とML詳細画面とML編集画面のスケルトンを作成
    /admin/ml, /admin/ml/list, /admin/ml/show/{id}, /admin/ml/edit/{id}
    のルーティング設定をした

--- module/Admin/config/module.config.php
+++ module/Admin/config/module.config.php
@@ -4,42 +4,62 @@
      'controllers' => array (
          'invokables' => array (
              'Admin\Controller\Index' => 'Admin\Controller\IndexController' ,
+             'Ml' => 'Admin\Controller\MlController' ,
          ),
      ),
 
      'router' => array (
          'routes' => array (
-             'admin' => array (
-                 'type' => 'segment' ,
-                 'options' => array (
-                     'route' => '/admin[/:action][/:id]' ,
-                     'constraints' => array (
-                         'action' => '[a-zA-Z][a-zA-Z0-9_-]*' ,
-                         'id' => '[0-9]+' ,
-                     ),
-                     'defaults' => array (
-                         'controller' => 'Admin\Controller\Index' ,
-                         'action' => 'index' ,
-                     ),
-                 ),
-             ),
-         ),
-     ),
 
-     'view_manager' => array (
-         'template_path_stack' => array (
-             'admin' => __DIR__ . '/../view' ,
-         ),
-     ),
+            'admin-ml' => array(
+                'type'    => 'Zend\Mvc\Router\Http\Literal',
+                'options' => array(
+                    'route'    => '/admin/ml',
+                    'constraints' => array(
+                        'controller' => 'Ml',
+                        'action'     => 'list',
+                    ),
+                    'defaults' => array(
+                        'controller' => 'Ml',
+                        'action'     => 'list',
+                    ),
+                ),
+                'may_terminate' => true,
+                'child_routes' => array(
+                    'has-id-action' => array(
+                        'type'    => 'Segment',
+                        'options' => array (
+                            'route' => '[/:action][/:id]' ,
+                            'constraints' => array (
+                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                'id' => '[0-9]+' ,
+                            ),
+                            'defaults' => array (
+                                'action' => 'show',
+                            ),
+                        ),
+                    ),
+                    'some-action' => array(
+                        'type'    => 'Segment',
+                        'options' => array(
+                            'route'    => '[/:action]',
+                            'constraints' => array(
+                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+                            ),
+                            'defaults' => array(
+                                'action' => 'list',
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+        ),
+    ),
 
-     'home' => array (
-         'type' => 'Zend\Mvc\Router\Http\Literal' ,
-         'options' => array (
-             'route' => '/' ,
-             'defaults' => array (
-                 'controller' => 'Admin\Controller\Index' ,    // Admin\Controller\AdminController ではない
-                 'action' => 'index' ,
-             ),
-         ),
-     ),
+    'view_manager' => array (
+        'template_path_stack' => array (
+            'admin' => __DIR__ . '/../view' ,
+        ),
+    ),
+
 );
