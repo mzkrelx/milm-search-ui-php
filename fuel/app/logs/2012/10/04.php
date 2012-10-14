5b3301f1aff4a54eadb0b0964a90e56171422bd7
Mizuki Yamanaka <charles.vineyard@gmail.com>
2012-10-14 23:50:34 +0900

    ログファイル削除

--- fuel/app/logs/2012/10/04.php
+++
@@ -1,5 +0,0 @@
-<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
-
-Info - 2012-10-04 00:34:49 --> Fuel\Core\Request::__construct - Creating a new Request with URI = "hello"
-Info - 2012-10-04 00:34:49 --> Fuel\Core\Request::execute - Called
-Info - 2012-10-04 00:34:49 --> Fuel\Core\Request::execute - Setting main Request
